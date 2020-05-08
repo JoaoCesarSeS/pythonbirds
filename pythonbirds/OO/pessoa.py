8492e16df25c26053a32f9dca3994f08031d0d98
joão cesar sousa e silva <cesar.joao@gmail.com>
2020-05-08 16:55:22 -0300

    Add files via upload

---
+++ pythonbirds/OO/pessoa.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    def __init__(self, nome = None, idade = None, sexo = None   ):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+    def cumprimentar(self):
+        return f'Olá '
+if __name__ == '__main__':
+    p = Pessoa('João',56)
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'César'
+    p.sexo = 'M'
+    print(p.cumprimentar(),p.nome, p.idade, p.sexo)
+    print ('Teste de commit', 7)
